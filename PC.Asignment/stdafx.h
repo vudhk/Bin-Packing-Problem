e0aaf983770ef6c8bfe02783aa8aa047c19ceda5
vudhk <vudhk@magik.vn>
2017-11-15 13:18:07 +0700

    Fix project

---
+++ PC.Asignment/stdafx.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "GeneticAlgorithm.h"
+#include "SmartFunc.h"
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+#include <random>
+#include <omp.h>
+#include <math.h>
+#include <stdio.h>
+
+#if true
+#define PARALLEL
+#else
+#define SEQUENTIAL
+#endif
