45ccb9aea20ac4732dab4a0db7bb9c3cc7572fee
vudhk <vudhk@magik.vn>
2017-11-17 13:13:55 +0700

    fix bug + completed GA

--- PC.Asignment/Assignment_v2/main.cpp
+++ PC.Asignment/Assignment_v2/main.cpp
@@ -2,52 +2,81 @@
 #include "main.h"
 
 using namespace std;
-using std::vector;
 
-#define MAX_POPULATION 100;
+#define MAX_POPULATION 100
 
+// global variable
 vector<box> *boxes;
 vector<individual> *population;
-int input_size;
+int input_size = 10;
+int bin_width = 20;
+int bin_height = 20;
+int bin_depth = 20;
 
-int main(int argv, char *argv[])
+// main function
+int main(int argv, char **argc)
 {
-	int i, num_of_box = 10;
+	int i;
 	srand(time(NULL));
-	boxes = new vector<box>[num_of_box];
+
+	cout << "Init the list of boxes..." << endl;
+	boxes = new vector<box>();
+	for (i = 0; i < input_size; i++)
+	{
+		box box(i);
+		box.width = random_int(1, 10);
+		box.height = random_int(1, 10);
+		box.depth = random_int(1, 10);
+		boxes->push_back(box);
+	}
+
+	cout << "Genetic algorithm is running..." << endl;
+	int solution = genetic_algorithm_run();
+	cout << "solution: " << solution << " bin(s)" << endl;
+
+	return 0;
 }
 
-
+// Genetic algorithm
 int genetic_algorithm_run()
 {
 	int i, j;
 	vector<individual> *new_population;
 	individual *father, *mother, *child;
 
+	cout << "Init population..." << endl;
 	init_population();
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 50; i++)
 	{
+		cout << "--- Iteration " << i + 1 << endl;
 		new_population = new vector<individual>();
-		for (j = 0; j < MAX_POPULATION / 2; j++)
+		for (j = 0; j < MAX_POPULATION; j++)
 		{
+			//cout << "--0: " << population->size() <<endl;
 			father = select_parent();
 			mother = select_parent();
+			//cout << "--1" <<endl;
 			child = crossover(father, mother);
 			mutation(child);
+			//cout << "--2" <<endl;
+			calc_fitness(child);
+			//cout << "--3" <<endl;
 			new_population->push_back(*child);
+			//cout << "--4" <<endl;
 		}
-
+		//cout << "--5" <<endl;
 		//double start = omp_get_wtime();
 		// delete old population
 		for (j = 0; j < MAX_POPULATION; j++)
 		{
-			delete &population->at(j);
+			//delete population;
 		}
-
+		//cout << "--6" << endl;
 		//double elapse = omp_get_wtime() - start;
 		// renew population
 		population = new_population;
 	}
+	cout << "Find the best fitness..." << endl;
 	return find_best_fitness();
 }
 
@@ -57,17 +86,18 @@ int find_best_fitness()
 	individual fitnist = population->at(0);
 	for (i = 1; i < MAX_POPULATION; i++)
 	{
-		if (population->at(i).fitness < fitnist->fitness)
+		if (population->at(i).fitness < fitnist.fitness)
 		{
 			fitnist = population->at(i);
 		}
 	}
-	return fitnist->fitness;
+	return fitnist.fitness;
 }
 
 void init_population()
 {
 	int i;
+	population = new vector<individual>();
 	for (i = 0; i < MAX_POPULATION; i++)
 	{
 		individual *idv = random_chromosome();
@@ -98,9 +128,43 @@ individual *select_parent()
 
 individual *crossover(individual *father, individual *mother)
 {
-	
-	
-	return father;
+	vector<BPS_gene> *bps_genes = new vector<BPS_gene>();
+	vector<BO_gene> *bo_genes = new vector<BO_gene>();
+	int j, k, r;
+	bool flag;
+	individual *parent;
+	for (j = 0; j < input_size; j++)
+	{
+		for (r = 0; r < 2; r++)
+		{
+			parent = r == 0 ? father : mother;
+			//cout << "id: " << parent->chrom->BPS->at(j).box_id << endl;
+			flag = false;
+			for (k = 0; k < bps_genes->size(); k++)
+			{
+				if (bps_genes->at(k).box_id == parent->chrom->BPS->at(j).box_id)
+				{
+					flag = true;
+					break;
+				}
+			}
+			if (!flag)
+			{
+				//cout << "--: " << parent->chrom->BPS->at(j).box_id << endl;
+				BPS_gene *bps_gen = new BPS_gene(parent->chrom->BPS->at(j).box_id);
+				bps_gen->value = parent->chrom->BPS->at(j).value;
+				bps_genes->push_back(*bps_gen);
+
+				BO_gene *bo_gene = new BO_gene();
+				bo_gene->value = parent->chrom->VBO->at(j).value;
+				bo_gene->bo = parent->chrom->VBO->at(j).bo;
+				bo_genes->push_back(*bo_gene);
+			}
+		}
+	}
+	//cout << "size: " << bo_genes->size() << endl;
+	individual *indivChild = new individual(new chromosome(bps_genes, bo_genes));
+	return indivChild;
 }
 
 void mutation(individual *idv)
@@ -110,96 +174,121 @@ void mutation(individual *idv)
 	j = random_int(0, input_size - 1);
 
 	// swap i-th idv's BPS-gene for j-th idv's BPS-gene
-	BPS_gene bps_gene_tmp = (*idv->chromosome->BPS)[i];
-	(*idv->chromosome->BPS)[i] = (*idv->chromosome->BPS[j]);
-	(*idv->chromosome->BPS)[j] = bps_gene_tmp;
+	BPS_gene bps_gene_tmp = (*idv->chrom->BPS)[i];
+	(*idv->chrom->BPS)[i] = (*idv->chrom->BPS)[j];
+	(*idv->chrom->BPS)[j] = bps_gene_tmp;
 
 	// swap i-th idv's BO-gene for j-th idv's BO-gene
-	BO_gene bo_gene_tmp = (*idv->chromosome->VBO)[i];
-	(*idv->chromosome->VBO)[i] = (*idv->chromosome->VBO[j]);
-	(*idv->chromosome->VBO)[j] = bo_gene_tmp;
+	BO_gene bo_gene_tmp = (*idv->chrom->VBO)[i];
+	(*idv->chrom->VBO)[i] = (*idv->chrom->VBO)[j];
+	(*idv->chrom->VBO)[j] = bo_gene_tmp;
 }
 
 individual *random_chromosome()
 {
-	int i, r;
+	int i,j,tmp;
 	vector<BPS_gene> *BPS = new vector<BPS_gene>();
 	vector<BO_gene> *VBO = new vector<BO_gene>();
+	int arr[input_size];
 	for (i = 0; i < input_size; i++)
 	{
-		r = random_int(0, input_size - 1);
-		BPS_gene bps_gene(boxes->at(r).id);
-		BO_gene bo_gene();
-		BPS->push_back(bps_gene);
-		VBO->push_back(bo_gene);
+		arr[i] = i;
 	}
-	chromosome *chromosome = new chromosome(BPS, VBO);
-	individual *idv = new individual(chromosome);
+	for (i = 0; i < input_size; i++)
+	{
+		j = random_int(1, input_size) - 1;
+		//swap
+		tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+	for (i = 0; i < input_size; i++)
+	{
+		BPS_gene *bps_gene = new BPS_gene(boxes->at(arr[i]).id);
+		BO_gene *bo_gene = new BO_gene();
+		BPS->push_back(*bps_gene);
+		VBO->push_back(*bo_gene);
+	}
+
+	chromosome *chrom = new chromosome(BPS, VBO);
+	individual *idv = new individual(chrom);
 }
 
 void calc_fitness(individual *idv)
 {
-	idv->fitness = placement(idv->chromosome);
+	idv->fitness = placement(idv->chrom);
 }
 
 // Đặt lần lượt các box vào bin
-int placement(chromosome *chromosome)
+int placement(chromosome *chrom)
 {
+	//cout << "placement-1" << endl;
 	// Initialization
 	int num_of_bin;
-	vector<int> bins();
-	bins.push_back(0);
+	vector<bin> *bins = new vector<bin>();
+	bins->push_back(*new bin());
 	int i, j, box_id, EMS_idx, bin_idx;
 	for (i = 0; i < input_size; i++)
 	{
+		//cout << "placement-1" << endl;
+
 		// Chọn box
-		box_id = chromosome->BPS->at(i).box_id;
+		box_id = chrom->BPS->at(i).box_id;
 		// Chọn Bin và EMS
 		EMS_idx = -1;
 		bin_idx = -1;
-		for (j = 0; j < bins.count(); j++)
+		for (j = 0; j < bins->size(); j++)
 		{
-			EMS_idx = dftrc_2(&boxes->at([chromosome->BPS->at(i).box_id]), &bins.at(j));
+			//cout << "placement-2" << endl;
+			EMS_idx = dftrc_2(&boxes->at(chrom->BPS->at(i).box_id), &bins->at(j));
+			//cout << "placement-3: " << EMS_idx << endl;
 			if (EMS_idx >= 0)
 			{
 				bin_idx = j;
 				break;
 			}
 		}
+		//cout << "placement-4" << endl;
+
 		if (bin_idx < 0)
 		{
 			// Mở một bin mới
-			bin_idx = bins.count();
-			bins.push_back(bin_idx);
+			bin_idx = bins->size();
+			bins->push_back(*new bin());
 			EMS_idx = 0;
 		}
-
+		//cout << "placement-5" << endl;
 		// Chọn hướng của box 0-đứng, 1-nằm
-		float bo_value = floor(chromosome->VBO->at(i).value * 2);
+		//cout << chrom->VBO->size() << endl;
+		float bo_value = floor(chrom->VBO->at(i).value * 2);
 		bo_value = bo_value >= 2 ? 1: bo_value;
+		//cout << "placement-5" << endl;
 		// Box packing
-		chromosome->BPS->at(i).flag = true;
-		chromosome->BPS->at(i).position.x = bins.at(bin_idx).EMSs->at(EMS_idx).position.x;
-		chromosome->BPS->at(i).position.y = bins.at(bin_idx).EMSs->at(EMS_idx).position.y;
-		chromosome->BPS->at(i).position.z = bins.at(bin_idx).EMSs->at(EMS_idx).position.z;
-		chromosome->VBO->at(i).value = bo_value;
+		chrom->BPS->at(i).flag = true;
+		chrom->BPS->at(i).position->x = bins->at(bin_idx).EMSs->at(EMS_idx).position->x;
+		chrom->BPS->at(i).position->y = bins->at(bin_idx).EMSs->at(EMS_idx).position->y;
+		chrom->BPS->at(i).position->z = bins->at(bin_idx).EMSs->at(EMS_idx).position->z;
+		chrom->VBO->at(i).value = bo_value;
 
-		bins.at(bin_idx).boxes->push_back(chromosome->BPS->at(i));
+		bins->at(bin_idx).boxes->push_back(chrom->BPS->at(i));
 
 		// Cập nhật thông tin trạng thái cho danh sách EMS
-		delete bins.at(bin_idx).EMSs
-		bins.at(bin_idx).EMSs = find_EMS(&bins.at(bin_idx));
+		delete bins->at(bin_idx).EMSs;
+		bins->at(bin_idx).EMSs = find_EMS(&bins->at(bin_idx));
+		//cout << "placement-6 : "<< i << endl;
 	} 
-	num_of_bin = bin.count();
-	delete &bins;
+	num_of_bin = bins->size();
+	delete bins;
+	//cout << "placement-7" << endl;
 	return num_of_bin;
 }
 
 // Return index of EMS in bin->EMSs
-// Return bo - hướng của box
 int dftrc_2(box *box, bin *bin)
 {
-	int num_of_EMS = bin->EMSs->count();
+	if (bin->EMSs == NULL)
+		return -1;
+	int num_of_EMS = bin->EMSs->size();
 	int EMS_idx = -1;
 	float maxDist = -1;
 	int i, j, k;
@@ -265,15 +354,14 @@ int dftrc_2(box *box, bin *bin)
 						}
 						break;
 				}
-
 				// if box fits i-th EMS of bin using BO
-				if (w <= bin->EMSs->at(EMS_idx).width &&
-					h <= bin->EMSs->at(EMS_idx).height &&
-					d <= bin->EMSs->at(EMS_idx).depth)
+				if (w <= bin->EMSs->at(i).width &&
+					h <= bin->EMSs->at(i).height &&
+					d <= bin->EMSs->at(i).depth)
 				{
-					float dist = pow(bin->width - bin->EMSs.at(EMS_idx).position.x - w) +
-								pow(bin->height - bin->EMSs.at(EMS_idx).position.y - h) +
-								pow(bin->depth - bin->EMSs.at(EMS_idx).position.z - d);
+					float dist = pow(bin->width - bin->EMSs->at(i).position->x - w, 2) +
+								pow(bin->height - bin->EMSs->at(i).position->y - h, 2) +
+								pow(bin->depth - bin->EMSs->at(i).position->z - d, 2);
 					if (dist > maxDist)
 					{
 						maxDist = dist;
@@ -289,7 +377,7 @@ int dftrc_2(box *box, bin *bin)
 
 vector<EMS> *find_EMS(bin *bin)
 {
-	return new vector<EMS>();
+	return new vector<EMS>(1, *new EMS());
 }
 
 int random_int(int start, int end)
@@ -299,8 +387,21 @@ int random_int(int start, int end)
 
 float random_float(float start, float end)
 {
-	return rand() % (end - start + 1) + start;
+	return (rand() % ((int)((end - start) * 1000) + 1)) / 1000.0 + start;
 }
+
+void print(vector<float> *vt)
+{
+	int i;
+	cout << "[" << vt->at(0); 
+	for (i = 1; i < vt->size(); ++i)
+	{
+		cout << ", " << vt->at(i);
+	}
+	cout << "]" << endl;
+}
+
+
 
 
 point::point()
@@ -326,11 +427,12 @@ EMS::EMS()
 }
 bin::bin()
 {
-	this->width = 0;
-	this->height = 0;
-	this->depth = 0;
-	this->boxes = NULL;
-	this->EMSs = NULL;
+	this->width = bin_width;
+	this->height = bin_height;
+	this->depth = bin_depth;
+	this->boxes = new vector<BPS_gene>();
+	this->EMSs = new vector<EMS>();
+	this->EMSs->push_back(*new EMS());
 	this->flag = false;
 }
 BPS_gene::BPS_gene(int box_id)
@@ -345,13 +447,15 @@ BO_gene::BO_gene()
 	this->bo = static_cast<box_orient>(random_int(0, 2));
 	this->value = random_float(0, 1);
 }
-chromosome::chromosome(BPS_gene *BPS_gene, BO_gene *BO_gene)
+chromosome::chromosome(vector<BPS_gene> *BPS_gene, vector<BO_gene> *BO_gene)
 {
 	this->BPS = BPS_gene;
 	this->VBO = BO_gene;
 }
-individual::individual(chromosome *chromosome)
+individual::individual(chromosome *chrom)
 {
-	this->chromosome = chromosome;
+	this->chrom = chrom;
 	this->fitness = -1;
 }
+
+
--- PC.Asignment/Assignment_v2/main.h
+++ PC.Asignment/Assignment_v2/main.h
@@ -4,17 +4,18 @@
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
-#include <random>
-#include <omp.h>
 #include <math.h>
 #include <stdio.h>
 #include <string>
+#include <stdlib.h>
+
+using std::vector;
 
 enum box_orient
 {
-	FRONT = 0
-	SIDE = 1
-	BOTTOM = 2
+	FRONT = 0,
+	SIDE = 1,
+	BOTTOM = 2,
 	UNKNOWN = 3
 };
 
@@ -45,17 +46,6 @@ struct EMS
 	EMS();
 };
 
-struct bin
-{
-	int width;
-	int height;
-	int depth;
-	vector<BPS_gene> *boxes;
-	vector<EMS> *EMSs;
-	bool flag;
-	bin();
-};
-
 struct BPS_gene
 {
 	int box_id;
@@ -70,22 +60,47 @@ struct BO_gene
 	box_orient bo;
 	float value;
 	BO_gene();
-}
+};
+
+struct bin
+{
+	int width;
+	int height;
+	int depth;
+	vector<BPS_gene> *boxes;
+	vector<EMS> *EMSs;
+	bool flag;
+	bin();
+};
 
 struct chromosome
 {
 	vector<BPS_gene> *BPS;
 	vector<BO_gene> *VBO;
-	chromosome();
+	chromosome(vector<BPS_gene> *BPS_gene, vector<BO_gene> *BO_gene);
 };
 
 struct individual
 {
-	chromosome *chromosome;
+	chromosome *chrom;
 	int fitness;
-	individual();
+	individual(chromosome *chrom);
 };
 
 vector<EMS> *find_EMS(bin *bin);
 int dftrc_2(box *box, bin *bin);
-void placement(chromosome *chromosome);
+int placement(chromosome *chrom);
+int genetic_algorithm_run();
+int find_best_fitness();
+void init_population();
+individual *select_parent();
+individual *crossover(individual *father, individual *mother);
+void mutation(individual *idv);
+individual *random_chromosome();
+void calc_fitness(individual *idv);
+int placement(chromosome *chrom);
+int dftrc_2(box *box, bin *bin);
+vector<EMS> *find_EMS(bin *bin);
+int random_int(int start, int end);
+float random_float(float start, float end);
+void print(vector<float> *vt);
